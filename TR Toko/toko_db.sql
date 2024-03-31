29246538cb82026c467a560c19088f2136023573
Nando <122329850+naando1@users.noreply.github.com>
2024-03-31 14:46:18 +0700

    Add files via upload

---
+++ TR Toko/toko_db.sql
@@ -0,0 +1,133 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 05, 2022 at 12:46 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `toko_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `ID_Barang` varchar(10) NOT NULL,
+  `Nama_Barang` varchar(50) DEFAULT NULL,
+  `Jenis` varchar(20) DEFAULT NULL,
+  `Ukuran` varchar(5) DEFAULT NULL,
+  `HargaBeli` decimal(10,0) DEFAULT NULL,
+  `HargaJual` decimal(10,0) DEFAULT NULL,
+  `Stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` varchar(50) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `PASSWORD` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `PASSWORD`) VALUES
+('AD001', 'anton', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `NoFaktur` varchar(20) NOT NULL,
+  `Tanggal` varchar(20) DEFAULT NULL,
+  `ID_Customer` varchar(10) DEFAULT NULL,
+  `TotalBeli` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`NoFaktur`, `Tanggal`, `ID_Customer`, `TotalBeli`) VALUES
+('TR0001', '04-12-2022', 'ID100', '22222'),
+('TR0002', '04-12-2022', 'ID001', '12000000'),
+('TR0003', '04-12-2022', 'ID002', '4000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualanrinci`
+--
+
+CREATE TABLE `penjualanrinci` (
+  `NoFaktur` varchar(20) DEFAULT NULL,
+  `ID_Barang` varchar(10) DEFAULT NULL,
+  `Nama_Barang` varchar(50) DEFAULT NULL,
+  `Jumlah` int(11) DEFAULT NULL,
+  `Harga` decimal(10,0) DEFAULT NULL,
+  `Total` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `penjualanrinci`
+--
+
+INSERT INTO `penjualanrinci` (`NoFaktur`, `ID_Barang`, `Nama_Barang`, `Jumlah`, `Harga`, `Total`) VALUES
+('TR0001', 'BR002', 'Bloods', 2, '150000', '300000'),
+('TR0002', 'BR002', 'Bloods', 3, '150000', '450000'),
+('TR0003', 'BR002', 'Bloods', 2, '150000', '300000'),
+('TR0001', 'BR001', 'Bloods', 2, '11111', '22222'),
+('TR0002', 'BR002', 'adidas', 6, '2000000', '12000000'),
+('TR0003', 'BR002', 'adidas', 2, '2000000', '4000000');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`ID_Barang`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`NoFaktur`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
